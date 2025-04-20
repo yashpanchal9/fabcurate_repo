15adaf801073d91bbc6de5bccb2901d981e7a4c7
nehainnovativeiteration <neha.innovativeiteration@gmail.com>
2025-04-20 21:15:35 +0530

    -change in ci.yml file

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
